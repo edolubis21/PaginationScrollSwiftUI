67b9f3f8246cda7fc5dc4eca73f48af886b8bdd3
edolubis21 <edolubis12345@gmail.com>
2024-03-12 23:00:50 +0700

    first commit

---
+++ PaginationScroll/PostModel.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  PaginationScroll
+//
+//  Created by edo lubis on 12/03/24.
+//
+
+import Foundation
---
+++ PaginationScroll/PostsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostsViewModel.swift
+//  PaginationScroll
+//
+//  Created by edo lubis on 12/03/24.
+//
+
+import Foundation
