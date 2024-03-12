cc2bd7db779b1ecff693c890fad2e2109e00dbfe
edolubis-ta <edo.lubis@tentanganak.id>
2024-03-12 21:06:11 +0700

    Initial Commit

---
+++ PaginationScroll/PaginationScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationScrollApp.swift
+//  PaginationScroll
+//
+//  Created by edo lubis on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PaginationScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
